e2a901290d9d0b189a1b578f6e181c61e8d613e2
Laelaelsaputri <114965835+Laelaelsaputri@users.noreply.github.com>
2022-10-04 09:10:07 +0700

    code

---
+++ chr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char nilai = 'r';
+    cout << "masuk nilai(a/b/c/d):";
+//    cin >> nilai;
+    cout << "nilai anda = " << nilai;
+    cout << endl;
+}
